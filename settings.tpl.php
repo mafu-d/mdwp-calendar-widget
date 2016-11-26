7d2fb630d25ea7eccf588a5a12953154c3b5ca46
Matthew Dawkins <matthew.dawkins@gmail.com>
2016-11-26 15:30:58 +0000

    Fixed missing PHP end tag

--- settings.tpl.php
+++ settings.tpl.php
@@ -4,7 +4,7 @@
 </p>
 <p>
     <label for="<?= $this->get_field_id('mode');?>">Mode</label>
-    <select class="widefat" id="<?= $this->get_field_id('mode')" name="<?= $this->get_field_name('mode');?>">
+    <select class="widefat" id="<?= $this->get_field_id('mode') ?>" name="<?= $this->get_field_name('mode');?>">
         <option value="week" <?=esc_attr($mode)==='week'?'selected':''?>>Week</option>
         <option value="month" <?=esc_attr($mode)==='month'?'selected':''?>>Month</option>
         <option value="year" <?=esc_attr($mode)==='year'?'selected':''?>>Year</option>
